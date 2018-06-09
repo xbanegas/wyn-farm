ff54652b3c0ef31429b300d141c1bad59877c35e
xbanegas <xbanegas@gmail.com>
2018-06-08 21:50:14 -0400

    more dry worldjs

--- src/components/World.js
+++ src/components/World.js
@@ -10,53 +10,58 @@ class World extends Component {
   constructor(props){
     super(props);
     this.state = initialData
-
-    this.makeWorldRows(this.state.world.size, this.state.player.location);
+    this.makeWorldRows(this.state.player.location);
     this.movePlayer = this.movePlayer.bind(this);
   }
   componentDidUpdate(){
     console.log("world update");
   }
 
-  makeWorldRows(size, playerLoc, player){
+  makeWorldRows(playerLoc, player){
     this.state.grid = makeGrid(this.state.world.size);
     this.these_rows = [];
     this.state.grid.forEach((row, i)=>{
-      this.these_rows.push(<Row key={i} styleName={`row row-${i}`} worldSize={this.state.world.size} playerLoc={playerLoc} rowNum={padNum(i)} />);
+      this.these_rows.push(
+        <Row 
+          key={i} 
+          styleName={`row row-${i}`} 
+          worldSize={this.state.world.size} 
+          playerLoc={playerLoc} 
+          rowNum={padNum(i)} />
+      );
     });
     this.setState({player: player});
-    // console.log(this.these_rows);
   }
   
   movePlayer(e){
     e.preventDefault();
+    let worldSize = this.state.world.size
     let player = {...this.state.player};
     let currentLoc = player.location;
-    // console.log(currentLoc);
+
     switch(e.key){
       case "w":
-        player.location = chngLocID(this.state.world.size, currentLoc, "vert", -1);
+        player.location = chngLocID(worldSize, currentLoc, "vert", -1);
         this.playerLoc = player.location;
         break;
       case "a":
-        player.location = chngLocID(this.state.world.size, currentLoc, "horiz", -1);
+        player.location = chngLocID(worldSize, currentLoc, "horiz", -1);
         this.playerLoc = player.location;
         break;
       case "s":
-        player.location = chngLocID(this.state.world.size, currentLoc, "vert", 1);
+        player.location = chngLocID(worldSize, currentLoc, "vert", 1);
         this.playerLoc = player.location;
         break;
       case "d":
-        player.location = chngLocID(this.state.world.size, currentLoc, "horiz", 1);
+        player.location = chngLocID(worldSize, currentLoc, "horiz", 1);
         this.playerLoc = player.location;
         break;
       }
-    this.makeWorldRows(this.state.world.size, this.playerLoc, player);
+    this.makeWorldRows(this.playerLoc, player);
     console.log(this.playerLoc);
   }
 
   render() {
-    // console.log(this.state.player.location);
     return (
       <div className="world" onKeyPress={this.movePlayer} tabIndex="0" >
         {this.these_rows}
